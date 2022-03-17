f00bfd237f68a56cf88992d58a0ff16332c8fca9
Marlon <Marlonnlmx@outlook.com>
2022-03-17 14:08:47 -0300

    Update

---
+++ Animal-Kingdom/src/app/Application.java
@@ -0,0 +1,34 @@
+package app;
+
+import app.src.entidades.SerVivo;
+import app.src.taxon.Reinos;
+
+public class Application {
+
+	public static void main(String[] args) { 
+		
+		// SerVivo(String nome, String especie, String genero, String familia, String ordem, String classe, String[] classificacao_adicional, String filo, String reino)
+		
+		// String[] add1 = {"Infraclasse: Placentalia", "Superordem: Xenarthra"};
+		// SerVivo SerVivo01 = new SerVivo("Tatu-bola", "Tolypeutes tricinctus", "Tolypeutes", "Clamiforídeos", "	Cingulata", "Mammalia", "Chordata", Reinos.Animalia);
+	
+		// SerVivo SerVivo02 = new SerVivo("Leopardo-das-neves", "Panthera uncia", "Panthera", "Felidae", "Carnivora", "Mammalia", null, "Chordata", "Animalia");
+		
+		// SerVivo SerVivo03 = new SerVivo("Lebre-do-ártico", "Lepus arcticus", "Lepus", "Leporidae", "Lagomorpha", "Mammalia", null, "Chordata", "Animalia");
+		
+		// String[] add2 = {"Subilo: Vertebrata"};
+		// SerVivo SerVivo04 = new SerVivo("Lebre-americana", "Lepus americanus", "Lepus", "Leporidae", "Lagomorpha", "Mammalia", add2, "Chordata", "Animalia");
+		
+		// String[] add3 = {"Subilo: Vertebrata", "Subordem: Feliformia", "Subclasse: Eutheria"};
+		// SerVivo SerVivo05 = new SerVivo("Lince-do-canadá", "Lynx canadensis", "Lynx", "Felidae", "Carnivora", "Mammalia", add3, "Chordata", "Animalia");
+		
+		// System.out.println(SerVivo01);
+		// System.out.println(SerVivo02);
+		// System.out.println(SerVivo03);
+		// System.out.println(SerVivo04);
+		// System.out.println(SerVivo05);
+		
+		
+		
+	}
+}
