c00f2a83b6acd8c89b90e59f3ba506e8cbaac3e5
Marlon <Marlonnlmx@outlook.com>
2022-03-19 16:45:18 -0300

    Launching code

---
+++ Animal-Kingdom/src/negocio/Registros.java
@@ -0,0 +1,32 @@
+package negocio;
+
+public interface Registros {
+	
+	public void RegistroA();
+	public void RegistroB();
+	public void RegistroC();
+	public void RegistroD();
+	public void RegistroE();
+	public void RegistroF();
+	public void RegistroG();
+	public void RegistroH();
+	public void RegistroI();
+	public void RegistroJ();
+	public void RegistroK();
+	public void RegistroL();
+	public void RegistroM();
+	public void RegistroN();
+	public void RegistroO();
+	public void RegistroP();
+	public void RegistroQ();
+	public void RegistroR();
+	public void RegistroS();
+	public void RegistroT();
+	public void RegistroU();
+	public void RegistroV();
+	public void RegistroW();
+	public void RegistroX();
+	public void RegistroY();
+	public void RegistroZ();
+	
+}
