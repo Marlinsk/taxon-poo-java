c36d8f5a519c9e23dc7329ccdaeb3e4198dea05c
Marlon <Marlonnlmx@outlook.com>
2022-03-20 15:45:58 -0300

    Launching code

---
+++ Animal-Kingdom/src/app/codex/Codex.java
@@ -0,0 +1,37 @@
+package app.codex;
+
+import app.negocio.ClassesTaxon;
+
+public class Codex implements ClassesTaxon {
+
+	@Override
+	public void Anfibios() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Artropodes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Aves() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Mamiferos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Repteis() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
