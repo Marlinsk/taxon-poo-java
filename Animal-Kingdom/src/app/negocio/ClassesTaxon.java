eb58e99b6c5f1f228e18755e3a36609c54aed645
Marlon <Marlonnlmx@outlook.com>
2022-04-03 00:52:12 -0300

    Launching code

---
+++ Animal-Kingdom/src/app/negocio/ClassesTaxon.java
@@ -0,0 +1,11 @@
+package app.negocio;
+
+public interface ClassesTaxon {
+	
+	public void Anfibios();
+	public void Artropodes();
+	public void Aves();
+	public void Mamiferos();
+	public void Repteis();
+	
+}
