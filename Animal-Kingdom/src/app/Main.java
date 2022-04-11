c18a96f1cca70127013adf4cf0d99c646e04c837
Marlon <Marlonnlmx@outlook.com>
2022-04-11 00:54:09 -0300

    Test

--- Animal-Kingdom/src/app/Main.java
+++ Animal-Kingdom/src/app/Main.java
@@ -1,27 +1,18 @@
 package app;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import app.entidades.Taxon;
-import app.funcoes.LerArquivos;
+import app.metodos.LerArquivos;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception { 
-		
-		try {
-			List<Taxon> artropodes = new ArrayList<>();
-			LerArquivos.LerArquivoArtropodesCSV(artropodes);
-			
-			for(int i = 0; i < artropodes.size(); i++) {
-				System.out.println(artropodes.get(i).toString());
-			}
-			
-		}catch (Exception e) {
-			throw e;
+		List<Taxon> artropodes = new ArrayList<>();
+		LerArquivos.LerArquivoArtropodes(artropodes);
+		for(int i = 0; i < artropodes.size(); i++) {
+			System.out.println(artropodes.get(i).toString());
 		}
-		
 	}
 }
