0190bd870fb562df3d6221aa05ef41d32b6b6071
Marlon <Marlonnlmx@outlook.com>
2022-04-21 00:01:33 -0300

    Update

--- Animal-Kingdom/src/app/codex/Codex.java
+++ Animal-Kingdom/src/app/codex/Codex.java
@@ -1,27 +1,34 @@
 package app.codex;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import app.LerArquivo;
+import app.entidades.Taxon;
+
 public class Codex implements Classes {
 
 	@Override
 	public void Artropodes() throws Exception {
-		
+		List<Taxon> Artropodes = new ArrayList<>();
+		LerArquivo.LerArquivoArtropodesCSV(Artropodes);
 	}
 
 	@Override
 	public void Aves() throws Exception {
-		// TODO Auto-generated method stub
-		
+		List<Taxon> Aves = new ArrayList<>();
+		LerArquivo.LerArquivoAvesCSV(Aves);
 	}
 
 	@Override
 	public void Mamiferos() throws Exception {
-		// TODO Auto-generated method stub
-		
+		List<Taxon> Mamiferos = new ArrayList<>();
+		LerArquivo.LerArquivoMamiferosCSV(Mamiferos);
 	}
 
 	@Override
 	public void Repteis() throws Exception {
-		// TODO Auto-generated method stub
-		
+		List<Taxon> Repteis = new ArrayList<>();
+		LerArquivo.LerArquivoRepteisCSV(Repteis);
 	}
 }
