041a34622e714618c83558c266dc19d665cc3e2b
Marlon <Marlonnlmx@outlook.com>
2022-04-20 23:31:29 -0300

    Launching code

---
+++ Animal-Kingdom/src/app/codex/Classes.java
@@ -0,0 +1,10 @@
+package app.codex;
+
+public interface Classes {
+	
+	public void Artropodes() throws Exception;
+	public void Aves() throws Exception;
+	public void Mamiferos() throws Exception;
+	public void Repteis() throws Exception;
+	
+}
---
+++ Animal-Kingdom/src/app/codex/Codex.java
@@ -0,0 +1,27 @@
+package app.codex;
+
+public class Codex implements Classes {
+
+	@Override
+	public void Artropodes() throws Exception {
+		
+	}
+
+	@Override
+	public void Aves() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Mamiferos() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Repteis() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
