412f7b9ec118f78444ade5962e78c7d713306fbb
Marlon <Marlonnlmx@outlook.com>
2022-04-21 00:00:32 -0300

    Update

--- Animal-Kingdom/src/app/Main.java
+++ Animal-Kingdom/src/app/Main.java
@@ -1,15 +1,8 @@
 package app;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import app.classes.Aves;
-import app.entidades.Taxon;
-
 public class Main {
 
 	public static void main(String[] args) throws Exception { 
-		List<Taxon> aves = new ArrayList<>();
-		Aves.listarAves(aves);
+		
 	}
 }
