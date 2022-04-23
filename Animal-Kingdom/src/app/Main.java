84378985e0f68958dcd25de7e4d2e67b59ed5ad0
Marlon <Marlonnlmx@outlook.com>
2022-04-23 19:30:00 -0300

    Adding code

--- Animal-Kingdom/src/app/Main.java
+++ Animal-Kingdom/src/app/Main.java
@@ -1,8 +1,21 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import app.entidades.Codex;
+import app.entidades.Taxon;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception { 
+		
+		Codex codex = new Codex();
+		List<Taxon> Aves = new ArrayList<>();
+		
+		LerArquivo.LerArquivoAvesCSV(Aves);
+		codex.getRegistros().todosRegistros(Aves);
+		codex.getFiltros().filtrarNomejuntoEspecie(Aves);
 		
 	}
 }
