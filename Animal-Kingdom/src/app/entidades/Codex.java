791ca4fc2931852f5afa505d2cc7d4ca2cd3cd3f
Marlon <Marlonnlmx@outlook.com>
2022-04-23 19:25:15 -0300

    Creating codex class

---
+++ Animal-Kingdom/src/app/entidades/Codex.java
@@ -0,0 +1,17 @@
+package app.entidades;
+
+import app.Filtros;
+import app.Registros;
+
+public class Codex {
+	
+	public Filtros filtros;
+	public Registros registros;
+	
+	public Filtros getFiltros() {
+		return filtros;
+	}
+	public Registros getRegistros() {
+		return registros;
+	}
+}
