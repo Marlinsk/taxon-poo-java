4958434b9348150c27cb40960c65c449b8d0a51b
Marlon <Marlonnlmx@outlook.com>
2022-04-23 19:31:50 -0300

    Changes

--- Animal-Kingdom/src/app/Filtros.java
+++ Animal-Kingdom/src/app/Filtros.java
@@ -8,8 +8,9 @@ public class Filtros {
 	
 	public static void filtrarNomejuntoEspecie(List<Taxon> classe) {
 		SelectSort.ordemAlfabeticaNomeEspecie(classe);
+		System.out.println("\nFitro com Nome e Espécie:");
 		for(int i = 0; i < classe.size(); i++) {
-			System.out.println("nome: " + classe.get(i).getNome() + ", espécie: " + classe.get(i).getEspecie());
+			System.out.println("Nome: " + classe.get(i).getNome() + ", Espécie: " + classe.get(i).getEspecie());
 		}
 	}
 }
